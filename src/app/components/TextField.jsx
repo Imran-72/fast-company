9c8b612c1f211e83222bea9dc61859a4dd0c6875
Imran Abdurakhmanov <petrow318@gmail.com>
2021-11-01 19:30:25 +0300

    Скрыть/показать пароль

--- src/app/components/TextField.jsx
+++ src/app/components/TextField.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import propTypes from "prop-types";
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const getInputClasses = () => {
     return "form-control" + (error ? " is-invalid" : "");
+  };
+
+  const toogleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
   };
   return (
     <div className="mb-4">
       <label htmlFor={name}>{label}</label>
-      <input
-        type={type}
-        id={name}
-        name={name}
-        value={value}
-        onChange={onChange}
-        className={getInputClasses()}
-      />
-      {error && <div className="invalid-feedback">{error}</div>}
+      <div className="input-group has-validation">
+        <input
+          type={showPassword ? "text" : type}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className={getInputClasses()}
+        />
+        {type === "password" && (
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={toogleShowPassword}
+          >
+            <i className={"bi bi-eye" + (showPassword ? "-slash" : "")}></i>
+          </button>
+        )}
+        {error && <div className="invalid-feedback">{error}</div>}
+      </div>
     </div>
   );
 };
