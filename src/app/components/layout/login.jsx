cf1724469c944729485c7dce8e35b0bbe6772f3f
Imran Abdurakhmanov <petrow318@gmail.com>
2021-11-01 09:49:14 +0300

    Базовая валидация

--- src/app/components/layout/login.jsx
+++ src/app/components/layout/login.jsx
@@ -1,23 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "../TextField";
 
 const Login = () => {
   const [data, setData] = useState({ email: "", password: "" });
+  const [, setErrors] = useState();
+
+  useEffect(() => {
+    validate();
+  }, [data]);
+  const validate = () => {
+    const errors = {};
+    for (const fieldName in data) {
+      if (data[fieldName] === "") {
+        errors[fieldName] = `${fieldName} обязятельно для заполнения`;
+      }
+    }
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validate();
+    if (!isValid) return;
+    console.log(data);
+  };
+
   const handleChange = ({ target }) => {
     setData((prevState) => ({
       ...prevState,
       [target.name]: [target.value],
     }));
-  };
-  setData((prevState) => ({
-    ...prevState,
-    [target.name]: [target.value],
-  }));
-};
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(data);
   };
   return (
     <form onSubmit={handleSubmit}>
